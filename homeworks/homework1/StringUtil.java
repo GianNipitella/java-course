0b6e48cc095d722fc642cab4a27e2afd5ef65777
Adrian Plattner <adrianplattner@gmail.com>
2013-07-26 17:49:50 -0430

    Template

---
+++ homeworks/homework1/StringUtil.java
@@ -0,0 +1,21 @@
+public class StringUtil{
+	public static String addCommasToNumericString(String digits){
+		return "";
+	}
+
+	public static String removeAllOccurrences(String str, char ch){
+		return "";
+	}
+
+	public static int countWordsOfAText(String text){
+		return 0;
+	}
+
+	public static String blow(String str){
+		return "";
+	}
+
+	public static int maxRun(String str){
+		return 0;
+	}
+}
