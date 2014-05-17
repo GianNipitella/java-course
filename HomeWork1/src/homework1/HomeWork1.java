e0037f41939a3cf543d82238791ade1aabed34f3
gnipitella <gnipitella@localhost.localdomain>
2014-05-16 21:42:49 -0430

    Casi terminada la tarea solo hace falta representar el Arbol Binario >.<

---
+++ HomeWork1/src/homework1/HomeWork1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package homework1;
+
+/**
+ *
+ * @author gnipitella
+ */
+public class HomeWork1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char caracter='1';
+        System.out.println(StringUtil.removeAllOccurrences("----0----0----",'-'));
+        System.out.println(StringUtil.addCommasToNumericString("10000000"));
+        System.out.println(StringUtil.countWordsOfAText("Hola     que tal como estas"));
+        System.out.println(StringUtil.blow("a3tx2z"));
+        System.out.println(StringUtil.maxRun("xyzx"));
+        
+    }   
+    
+}
---
+++ HomeWork1/src/homework1/StringUtil.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package homework1;
+
+/**
+ *
+ * @author gnipitella
+ */
+public class StringUtil{
+	public static String addCommasToNumericString(String digits){
+                //revisar si es un numero
+        
+            String ls_cadena= digits;
+            String newCadena="";
+            String cadenaFinal="";
+            int longitudCadena=0;
+            int contador=0;
+            int longitudNewCadena=0;
+            longitudCadena=ls_cadena.length();
+//            System.out.println(longitudCadena);
+//            System.out.println(ls_cadena);
+            for (int i=longitudCadena; i>0; i--){
+                contador=contador+1;
+//                System.out.println("contador vale:"+contador);
+                    if(contador==3 && i>1){
+                       newCadena=newCadena+ls_cadena.charAt(i-1)+",";
+                       contador=0;
+                       }               
+                    else{ 
+                         newCadena=newCadena+ls_cadena.charAt(i-1);                              
+                       }           
+               }
+            longitudNewCadena=newCadena.length();
+//            System.out.println(longitudNewCadena);
+//            System.out.println(newCadena);
+            for (int j=longitudNewCadena; j>0; j--){
+//                System.out.println("i vale:"+j);
+                cadenaFinal=cadenaFinal+newCadena.charAt(j-1);
+            }
+//            System.out.println(longitudNewCadena);
+//            System.out.println("Cadenafinal vale:"+cadenaFinal);
+            return cadenaFinal;
+	}
+
+	public static String removeAllOccurrences(String str, char ch){
+            String cadenaPrimera=str;
+            String nuevaCadena="";
+            char remobable=ch;
+            char caracter;
+            int longitudCadenaPrimera=0;
+            
+            longitudCadenaPrimera=cadenaPrimera.length();
+            for (int i=0; i<longitudCadenaPrimera; i++){
+            caracter=cadenaPrimera.charAt(i);
+                if(caracter==remobable){
+                
+                }
+                else{
+                nuevaCadena=nuevaCadena+cadenaPrimera.charAt(i);
+                }
+
+            
+            
+            }
+            
+		return nuevaCadena;
+	}
+
+	public static int countWordsOfAText(String text){
+            
+            String cadenaPrimera=text;
+            String nuevaCadena="";
+            char prevCaracter='#';
+            char caracter;
+            int longitudCadenaPrimera=0;
+            int contador=0;
+            longitudCadenaPrimera=cadenaPrimera.length();
+            for (int i=0; i<longitudCadenaPrimera; i++){
+            caracter=cadenaPrimera.charAt(i);
+            
+             
+            if (i>0){
+            prevCaracter=cadenaPrimera.charAt(i-1);
+            }
+                if(caracter==' ' || caracter=='\t' || caracter=='\n' || caracter=='\r' || caracter=='\u0000'){
+                    if(prevCaracter!=' ' && prevCaracter!='\t' && prevCaracter!='\n' && prevCaracter!='\r' && prevCaracter!='\u0000'){
+                contador=contador+1;
+                    }
+                }
+                if(longitudCadenaPrimera==i+1 && prevCaracter!=' ' && prevCaracter!='\t' && prevCaracter!='\n' && prevCaracter!='\r' && prevCaracter!='\u0000'){
+                 contador=contador+1;
+                }
+            //System.out.println(i+":"+caracter+":"+contador+":"+prevCaracter);
+            }
+            
+		return contador;
+	}
+
+	public static String blow(String str){
+            String cadenaPrimera=str;
+            String nuevaCadena="";
+            String cadenaFinal;
+            int valorDigito=0;
+            int longitudCadenaPrimera=0;
+            longitudCadenaPrimera=cadenaPrimera.length();
+            char caracter ='#';
+            
+            
+            for(int i=0; i<longitudCadenaPrimera;i++){
+                //System.out.println("interaccion numero:"+i);
+                caracter=cadenaPrimera.charAt(i);
+                if(Character.isDigit(caracter)==true){
+                   // System.out.println("es Digito:"+i);
+                   valorDigito=Character.digit(caracter , 10);
+                   //System.out.println("el Valor es:"+valorDigito);
+                   for (int j=1; j<valorDigito;j++){
+                   //System.out.println("lo Repito"+j+" veces");
+                   nuevaCadena=nuevaCadena+cadenaPrimera.charAt(i+1);
+                   }
+                }
+                else{
+                     //System.out.println("no es Digito:"+i);
+                nuevaCadena=nuevaCadena+cadenaPrimera.charAt(i);
+                }
+                
+                }
+                
+            
+            
+            cadenaFinal=nuevaCadena.trim();
+		return cadenaFinal;
+	}
+
+	public static int maxRun(String str){
+            String cadena=str;
+            int longitudCadena=cadena.length();
+            int contadorPrimario=1;
+            int contadorFinal=1;
+            for (int i=0;i<longitudCadena-1;i++){
+                ;
+                Character caracterAComparar=cadena.charAt(i);
+                Character siguienteCaracter=cadena.charAt(i+1);
+                //System.out.println("es "+caracterAComparar+"= "+siguienteCaracter+"?");
+                if (caracterAComparar.equals(siguienteCaracter)){
+                    //System.out.println("Si es agrego uno al contadorPrimario");
+                    contadorPrimario=contadorPrimario+1;
+                    //System.out.println(contadorPrimario+">"+contadorFinal+"?");
+                    if(contadorPrimario>contadorFinal){
+                       // System.out.println("Si es Almaceno el contador primario en el contador final");
+                    contadorFinal=contadorPrimario;
+                    }
+
+
+                }
+                else {
+                    contadorPrimario=1;
+                }
+            }
+            
+		return contadorFinal;
+	}
+}
